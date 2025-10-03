8e552b95676481417b82612a2eb4713a13068478
Pablo André <macedopablo2008@gmail.com>
2025-10-03 15:59:21 -0300

    ETAPA 2 - Def Cadastro de livros

--- main.py
+++ main.py
@@ -13,3 +13,27 @@ CREATE TABLE IF NOT EXISTS livros (
     disponibilidade CHAR(1) CHECK(disponibilidade IN('S','N'))
     )
 """)
+
+def add_livro(nome_livro,autor_livro,ano):
+    try:
+        conexao = sqlite3.connect("biblioteca.db")
+        cursor = conexao.cursor()
+
+        cursor.execute("""
+        INSERT INTO livros(nome_livro, autor_livro, ano, disponibilidade) VALUES (?,?,?,?)""", (nome_livro, autor_livro, ano, "S"))
+
+        conexao.commit()
+
+        if cursor.rowcount > 0:
+            print(f"Livro {nome_livro} foi cadastrado com sucesso!")
+
+        else:
+            print("Erro ao cadastrar o Livro!")
+
+    except sqlite3.Error as error:
+        print("Erro ao cadastrar o livro {error}")
+
+    finally:
+        if conexao:
+            conexao.close()
+            
